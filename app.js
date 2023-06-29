91bb06e99989ba57c6a069f5ef6474c966d33a8c
frkntkny <80395899+frkntkny@users.noreply.github.com>
2023-06-29 17:20:11 +0100

    mobil alignment bug fixed

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 
+// removee the class container in the div id divShow1 if the uesr comes from mobil device
+
+if (/Mobi/.test(navigator.userAgent)) {
+    const divShow1 = document.getElementById("divShow1");
+    divShow1.classList.remove("container");
+    divShow1.style.width = "95%";
+    divShow1.style.margin = "0 auto";
+    divShow1.style.fontSize = "2.7rem";
+  }
