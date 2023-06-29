f8bcf66b0c312f4eee66638ec16aed90944c8016
frkntkny <80395899+frkntkny@users.noreply.github.com>
2023-06-29 19:36:16 +0100

    font size for mobile view increased

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ if (/Mobi/.test(navigator.userAgent)) {
   divShow1.style.fontSize = "2.7rem";
   //get the id link1
   const link1 = document.getElementById("link1");
-  link1.style.fontSize = "2.0rem";
+  link1.style.fontSize = "2.7rem";
   link1.style.color = "white";
   link1.style.borderColor = "white";
   link1.style.height = "70px";
@@ -24,7 +24,7 @@ if (/Mobi/.test(navigator.userAgent)) {
 
   //get the id link2
   const link2 = document.getElementById("link2");
-  link2.style.fontSize = "2.0rem";
+  link2.style.fontSize = "2.7rem";
   link2.style.color = "white";
   link2.style.borderColor = "white";
   link2.style.height = "70px";
@@ -40,7 +40,7 @@ if (/Mobi/.test(navigator.userAgent)) {
 
   //get the id link3
   const link3 = document.getElementById("link3");
-  link3.style.fontSize = "2.0rem";
+  link3.style.fontSize = "2.7rem";
   link3.style.color = "white";
   link3.style.borderColor = "white";
   link3.style.height = "70px";
